1880d5e1c58bf705aa6f794eb56bea101ef52b75
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2023-12-10 09:44:03 -0300

    utils ImageUpload

---
+++ backend/controllers/ImageController.js
@@ -0,0 +1,6 @@
+const Image = require('../models/Image')
+
+const insertImage = async(req,res)=>{
+    
+}
+module.exports={}
--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,8 @@ app.use(express.urlencoded({
     extended: true 
 }))
 
+app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
+
 app.use(express.json())
 
 app.listen(5000,()=>{
