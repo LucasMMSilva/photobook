5dacf79f686f9a71a2afb0e78fb9cda1e6bb2a04
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2023-12-10 10:30:19 -0300

    insert image

--- backend/controllers/ImageController.js
+++ backend/controllers/ImageController.js
@@ -1,6 +1,17 @@
 const Image = require('../models/Image')
 
 const insertImage = async(req,res)=>{
-    
+    const {title,description} = req.body
+    const images = req.files
+    const newImages = await Image.create({
+        title,
+        description,
+        images
+    })
+    if(!newImages){
+        res.status(422).json({msg:'Algo de errado aconteceu!'})
+        return
+    }
+    res.status(201).json(newImages)
 }
-module.exports={}
+module.exports={insertImage}
