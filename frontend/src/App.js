19daa017892d4e1fdbf1ba782f47644901bbd589
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2023-12-12 18:49:29 -0300

    Configurando frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      React
+    </div>
+  );
+}
+
+export default App;
