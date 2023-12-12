6420c8b1a4c1f0a228260510d26e231f21569359
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2023-12-12 19:20:26 -0300

    navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -3,7 +3,10 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <nav>
-        <img src="/logopb.png" alt="" />
+        <img src="/logopb.png" alt="Logo Photobook"/>
+        <div>
+            <button>Enviar Imagem</button>
+        </div>
     </nav>
   )
 }
