02cbd242936cd920b80fcbbfbf7301f04644ff00
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2023-12-12 17:15:19 -0300

    Editando Imagem

--- backend/controllers/ImageController.js
+++ backend/controllers/ImageController.js
@@ -1,23 +1,15 @@
 const fs = require('fs')
-const path = require('path')
 
 const Image = require('../models/Image')
 
 function removeImages (e){
-
-    /*fs.unlink(path.join('public/images/1702331700399.jpg'),(err)=>{
-        console.log(err)
-    })*/
-
     e.forEach(element => {
         fs.unlink(element.path,(err)=>{
             if(err){
                 console.log(element.filename + ' não foi encontrado!')
             }
-            
         })
     });
-
 }
 
 const InsertImage = async(req,res)=>{
@@ -70,7 +62,43 @@ const deleteImageById = async(req,res)=>{
         return
     }
     removeImages(image.images)
-    res.status(200).json({msg:"Imagem apagada com sucesso."})
-    
+    res.status(200).json({msg:"Imagem apagada com sucesso."}) 
 }
-module.exports={InsertImage,getAllImages,getImageById,deleteImageById}
+const imageUpdateById = async (req,res)=>{
+    const {id} = req.params
+    const {title,description} = req.body
+    const images = req.files
+    let updateData = {}
+    
+    const image = await Image.findById(id)
+    if(!image){
+        res.status(422).json({msg:'Imagem não existe'})
+        return
+    }
+    if(!title){
+        res.status(422).json({msg:'Titulo é obrigatorio.',type:'TITLE NULL'})
+        return
+    }else(
+        updateData.title = title
+    )
+    if(!description){
+        res.status(422).json({msg:'Descrição é obrigatoria.',type:'DESCRIPTION NULL'})
+        return
+    }else(
+        updateData.description = description
+    )
+    if(!images){
+        res.status(422).json({msg:'Imagem é obrigatoria.',type:'IMAGE NULL'})
+        return
+    }else{
+        updateData.images = []
+        images.map((image) => {
+            updateData.images.push(image)
+        })
+    }
+    const newImage = await Image.findByIdAndUpdate(id,updateData)
+    removeImages(image.images)
+    res.status(200).json(newImage)
+}
+
+module.exports={InsertImage,getAllImages,getImageById,deleteImageById,imageUpdateById}
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const router = express.Router()
-const {InsertImage,getAllImages,getImageById,deleteImageById} = require('../controllers/ImageController')
+const {
+    InsertImage,
+    getAllImages,
+    getImageById,
+    deleteImageById,
+    imageUpdateById
+} = require('../controllers/ImageController')
+
 const {uploadImage} = require('../utils/uploadImage')
 
 router.get('/',getAllImages)
 router.get('/:id',getImageById)
 router.post('/insertImage',uploadImage.array('images'),InsertImage)
+router.put('/edit/:id',uploadImage.array('images'),imageUpdateById)
 router.delete('/delete/:id',deleteImageById)
 
 module.exports = router
