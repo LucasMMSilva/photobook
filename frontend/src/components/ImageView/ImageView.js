acf4466fc5330d231135e987baf68bc8606bd2a9
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2023-12-14 20:59:02 -0300

    image view

--- frontend/src/components/ImageView/ImageView.js
+++ frontend/src/components/ImageView/ImageView.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './ImageView.css'
+import { AiOutlineClose } from "react-icons/ai";
+import { FiEdit } from "react-icons/fi";
+import { RiDeleteBin5Line } from "react-icons/ri";
+
 const ImageView = () => {
   return (
     <div className='bgf'>
@@ -7,10 +11,18 @@ const ImageView = () => {
             <div className="image-view">
 
             </div>
-            <div className="description">
-                <h3>Este é o titulo imagem</h3>
-                <p>Esta aqui é uma paquena descrição da imagem, para dar enfaze em alguma coisa ou sei lá o que.</p>
+            <div className="image-content">
+                <div className="description">
+                    <h3>Este é o titulo imagem</h3>
+                    <p>Esta aqui é uma paquena descrição da imagem, para dar enfaze em alguma coisa ou sei lá o que.</p>
+                </div>
+                <div className='image-icons'>
+                    <AiOutlineClose />
+                    <FiEdit />
+                    <RiDeleteBin5Line />
+                </div>
             </div>
+            
         </div>
     </div>
   )
