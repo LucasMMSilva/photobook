7350a7e13394e4e7c350c76f19e5ef1acae22e78
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2024-01-29 22:43:40 -0300

    Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Navbar from './Components/layout/Navbar/Navbar';
+import Footer from './Components/layout/Footer/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<div>oi</div>}/>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
