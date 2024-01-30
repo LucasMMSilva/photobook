6aa0583dba2dd12171438c5c1d98a79560c13e03
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2024-01-29 23:01:45 -0300

    mainContainer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Navbar from './Components/layout/Navbar/Navbar';
 import Footer from './Components/layout/Footer/Footer';
+import MainContainer from './Components/layout/MainContainer/MainContainer';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar/>
-        <Routes>
-          <Route path='/' element={<div>oi</div>}/>
-        </Routes>
+        
+        <MainContainer>
+          <Routes>
+            <Route path='/' element={<div>oi</div>}/>
+          </Routes>
+        </MainContainer>
+        
         <Footer/>
       </BrowserRouter>
     </div>
