5b6e1c85e4053881ade39d5275b575d16ac49fd8
Your Name <86989674+LucasMMSilva@users.noreply.github.com>
2024-03-14 17:22:30 -0300

    Removendo o frontend

--- frontend/src/Components/layout/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from './Footer.module.css'
-const Footer = () => {
-  return (
-    <div className={styles.footer}>Photobook - digital photo album</div>
-  )
-}
-
-export default Footer
